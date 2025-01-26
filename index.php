63e7842720fd59e14ebd154a52de33e061c293db
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-01-26 10:01:41 +0100

    Update

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- Page d'accueil -->
+<?php include_once "includes/header.php"; ?>
+
+<body>
+    <main class="container py-5">
+        <div class="container-fluid">
+            <h1 class="text-center fw-bold fs-2 mb-4">Bienvenue sur le blog d'Aqua Passion</h1><br>
+            <p class="justified-text mt-4">
+                Votre espace dédié à la natation ! <br><br>
+                Que vous soyez un nageur chevronné ou un débutant souhaitant
+                découvrir les joies de l'eau, vous trouverez ici de nombreux conseils pour améliorer votre technique, vous entraîner efficacement
+                et vous maintenir en forme. <br><br>
+                De la brasse au crawl en passant par le papillon, nous explorerons toutes les nages et vous partagerons
+                nos astuces pour progresser rapidement.
+            </p>
+            <p class="justified-text mt-4">
+                <strong>Alors, enfilez votre maillot de bain et plongez avec nous dans l'univers de la natation !</strong>
+            </p><br><br>
+            <div class="d-flex justify-content-center">
+                <div class="border border-5 rounded p-2">
+                    <img class="img-fluid mx-auto d-block" style="max-width: 700px" src="/assets/img/accueil/natation.webp" alt="Natation">
+                </div>
+            </div><br>
+        </div>
+    </main>
+
+
+    <?php include_once "includes/footer.php"; ?>
