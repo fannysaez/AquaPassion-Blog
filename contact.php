20c46e8c6d94c27da7d1b20dc7bf5bb189f1b406
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-01-26 10:07:01 +0100

    Update

---
+++ contact.php
@@ -0,0 +1,15 @@
+<!-- Page contact -->
+<?php include_once "includes/header.php"; ?>
+
+<body>
+    <main class="container py-5 mb-3">
+        <h1 class="text-center fw-bold fs-3 mb-5">Nagez, explorez, dominez les vagues</h1>
+        <div class="d-flex justify-content-center">
+            <div class="border border-5 rounded p-3">
+                <img class="img-fluid mx-auto d-block" src="/assets/img/contact/surf.webp" alt="Surf" style="max-width: 1000px">
+            </div>
+        </div>
+    </main>
+</body>
+
+<?php include_once "includes/footer.php"; ?>
