69caaa2be56e23b0869842af9172d850ba167724
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-01-26 10:05:02 +0100

    Update

---
+++ includes/footer.php
@@ -0,0 +1,27 @@
+<!-- page footer -->
+<footer class="footer bg-light">
+    <div class="text-center d-flex justify-content-around">
+
+        <p class="mb-0">
+            <i class="bi bi-geo-alt text-primary fs-3"></i><br><br>
+            123 Rue de l'Océan,<br>75001 Ville Natation<br>
+        </p>
+        <p class="mb-0">
+            <i class="bi bi-telephone text-primary fs-3"></i><br><br>
+            <a href="tel:+33123456789">+33 1 23 45 67 89</a><br>
+        </p>
+
+        <p class="mb-0">
+            <i class="bi bi-envelope text-primary fs-3"></i><br><br>
+            <a class="btn-primary-link" href="mailto:contact@aquapassion.com">contact@aquapassion.com</a>
+        </p>
+
+    </div>
+</footer>
+
+<script src="/assets/js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!-- page de navigation -->
+<!DOCTYPE html>
+<html lang="fr">
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aqua Blog</title>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Raleway:wght@400;700&family=Pacifico&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/assets/css/styles.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+
+<body>
+  <nav id="navbar-example2" class="navbar navbar-light bg-light px-2">
+    <a class="navbar-brand" href="">
+      <img class="logo" src="/assets/img/logo/AquaPassion.svg" alt="logo AquaPassion">
+    </a>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+  </nav>
