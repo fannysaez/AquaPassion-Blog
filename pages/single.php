2038b4d59480c2528521bc8ed205c47f5303b8ea
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-01-27 14:55:52 +0100

    initial commit

--- pages/single.php
+++ pages/single.php
@@ -7,16 +7,14 @@ $content = $articles[$id];
 <main class="container py-5 mb-3">
 
     <div class="container-fluid">
-        <h1 class="text-center py-2 mb-4"><?php echo $content["name"]; ?>
-        </h1>
-
         <div class="card">
-            <h5 class="card-header"><?php echo $content["createdBy"]; ?></h5>
+            <h1 class="card-header text-center py-2 mb-4"><?php echo $content["name"]; ?></h1>
             <img src="../assets/img/blog/<?php echo $content["imagePath"]; ?>" style="max-width: 600px" class="card-img-top rounded img-fluid mx-auto d-block py-5" alt="<?php echo $articles[$id]["name"]; ?>">
             <div class="card-body">
                 <p class=" lh-lg card-description text-start"><?php echo $content["article"]; ?></p>
 
                 <p class="card-date text-end"><?php echo $content["date"]; ?></p>
+                <p class="card-date text-end"> <?php echo $content["createdBy"]; ?></p>
             </div>
         </div>
     </div>
