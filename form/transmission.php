77c83eedc5df669483681581c1b8a916b5a11371
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-01-28 13:20:32 +0100

    Update

--- form/transmission.php
+++ form/transmission.php
@@ -1,26 +1,32 @@
 <?php include_once "includes/header.php"; ?>
 
-<?php
+<!-- <?php
 if (isset($_POST['nom']) && isset($_POST['email'])) {
     $nom = $_POST['nom'];
     $email = $_POST['email'];
-    // echo  "<p>" .  $nom . "</p>";
-    // echo "<p>" .  $email . "</p>";
 }
-?>
+?> -->
 <main class="container mt-5 py-5 mb-3">
     <div class="container-fluid">
         <div class="card">
             <h1 class="card-header text-center fw-bold fs-3">Données du formulaire</h1>
-            <div class="card-body">
-                <h5 class="card-title"></h5>
-                <p class="card-text"></p>
+            <?php if(isset($_POST['nom']) && isset($_POST['email']) && isset($_POST['sujet']) && isset($_POST['message'])); ?>
+            <div class="alert alert-success" role="alert">
+                Votre message à bien été envoyer avec succés !
+
             </div>
+            <p><b>Nom :</b><br> <?php echo $_POST["nom"] ?> </p>
+            <p><b>Email :</b><br> <?php echo $_POST["email"] ?> </p>
+            <p><b>Sujet :</b><br>  <?php echo $_POST["sujet"] ?> </p>
+            <p class="message-box"><b>Message :</b> <br> <?php echo $_POST["message"] ?> </p>
+            
         </div>
     </div>
-    <div class="text-center mt-5 py-5 mb-3">
+
+    <div class="text-center mt-3 py-3 mb-3">
     <a href="../pages/contact.php" class="btn btn-primary">Retour au formulaire</a>
     </div>
+
 </main>
 
 
