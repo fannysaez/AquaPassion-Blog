99e6d334899d2a42b3097771094e360b2136461a
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-01-28 14:11:46 +0100

    Update

--- form/transmission.php
+++ form/transmission.php
@@ -6,15 +6,17 @@
         <div class="card">
             <h1 class="card-header text-center fw-bold fs-3">Données du formulaire</h1>
             <?php if (isset($_POST['nom']) && isset($_POST['email']) && isset($_POST['sujet']) && isset($_POST['message'])): ?>
-                <div class="alert alert-success" role="alert">
+                <div class="alert alert-success text-center" role="alert">
                     Votre message à bien été envoyer avec succés !
                 </div>
-                <p class=""><b>Nom :</b><br> <?php echo $_POST["nom"] ?> </p>
-                <p class=""><b>Email :</b><br> <?php echo $_POST["email"] ?> </p>
-                <p class=""><b>Sujet :</b><br> <?php echo $_POST["sujet"] ?> </p>
-                <p class="message-box"><b>Message :</b> <br> <?php echo $_POST["message"] ?> </p>
+                <div class="mx-3 p-3">
+                <p><b>Nom :</b><br> <?php echo $_POST["nom"] ?> </p>
+                <p><b>Email :</b><br> <?php echo $_POST["email"] ?> </p>
+                <p><b>Sujet :</b><br> <?php echo $_POST["sujet"] ?> </p>
+                <p class="message-box my-5"><b>Message :</b> <br> <?php echo $_POST["message"] ?> </p>
+                </div>
             <?php else: ?>
-                <div class="alert alert-danger" role="alert">
+                <div class="alert alert-danger text-center" role="alert">
                 Aucune donnée n'a été trouvée !!
                 </div>
 
